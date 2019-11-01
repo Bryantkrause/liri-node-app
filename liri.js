54075d8deac035b3033e0d0d14eaf709f3923d51
Bryant Krause <bkrause@ttdwest.local>
2019-11-01 14:54:36 -0700

    setting up intitial npm and api

--- liri.js
+++ liri.js
@@ -1,1 +1,27 @@
+require("dotenv").config();
 var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+artist = 'inferi'
+var spotify = new Spotify(keys.spotify);
+
+// spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
+   
+//   console.log(data); 
+//   });
+fetch(`"https://rest.bandsintown.com/artists/"+${artist}+"/events?app_id=codingbootcamp"`)
+.then(data => data.json())
+.then(data => {
+    console.log(data)
+})
+// Make it so liri.js can take in one of the following commands:
+
+//    * `concert-this`
+
+//    * `spotify-this-song`
+
+//    * `movie-this`
+
+//    * `do-what-it-says`
