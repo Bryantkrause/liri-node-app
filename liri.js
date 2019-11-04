38e04198337220e14618216c22c0911fee4f4f21
Bryant Krause <bkrause@ttdwest.local>
2019-11-04 14:30:27 -0800

    added initial menu and switch case frame

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios");
+var inquirer = require("inquirer");
 var Spotify = require('node-spotify-api');
 artist = 'inferi'
 var spotify = new Spotify(keys.spotify);
@@ -26,3 +27,30 @@ axios.get(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbo
 //    * `movie-this`
 
 //    * `do-what-it-says`
+
+let myMenu = () =>{
+inquirer.prompt({
+    type: 'list',
+    name: 'options',
+    message: 'Choose an option.',
+    choices: [`concert-this`,`spotify-this-song`,`movie-this`,`do-what-it-says`]
+})
+.then(({options})=>{
+    switch (options){
+        case `concert-this`:
+            console.log(`concert-this`)
+            break
+            case `spotify-this-song`:
+            console.log(`spotify-this-song`)
+            break
+            case `movie-this`:
+            console.log(`movie-this`)
+            break
+            case `do-what-it-says`:
+            console.log(`do-what-it-says`)
+            break
+            
+    }
+})
+.catch(e => console.log(e))
+}
