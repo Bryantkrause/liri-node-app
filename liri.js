15841beb2113a559da64384f733650839e878574
Bryant Krause <bkrause@ttdwest.local>
2019-11-04 11:14:44 -0800

    using band in town api

--- liri.js
+++ liri.js
@@ -11,11 +11,18 @@ var spotify = new Spotify(keys.spotify);
    
 //   console.log(data); 
 //   });
-fetch(`"https://rest.bandsintown.com/artists/"+${artist}+"/events?app_id=codingbootcamp"`)
+fetch(`https://rest.bandsintown.com/artists/${artist}/events?app_id=codingbootcamp`)
 .then(data => data.json())
 .then(data => {
     console.log(data)
 })
+let url = `https://rest.bandsintown.com/artists/${artist}?app_id=codingbootcamp`
+curl -X GET "https://rest.bandsintown.com/artists/inferi?app_id=codingbootcamp" -H "accept: application/json"
+// fetch(`"https://rest.bandsintown.com/artists/"+${artist}+"/events?app_id=codingbootcamp"`)
+// .then(data => data.json())
+// .then(data => {
+//     console.log(data)
+// })
 // Make it so liri.js can take in one of the following commands:
 
 //    * `concert-this`
